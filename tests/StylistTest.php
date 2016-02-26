08ff08c80243e1ed7f8fda26ad15173765a7234c
Blake Scott <Blakerscott@gmail.com>
2016-02-26 11:05:05 -0800

    add tests for stylist class save, get all and delete all methods

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -1,16 +1,73 @@
 <?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
 
-     require_once "src/Stylist.php";
+    require_once "src/Client.php";
+    require_once "src/Stylist.php";
 
-     class StylistTest extends PHPUnit_Framework_TestCase
+    $server = 'mysql:host=localhost;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+     class CuisineTest extends PHPUnit_Framework_TestCase
      {
 
-         function test_method_breakitdown()
+        protected function tearDown()
+        {
+          // Client::deleteAll();
+          Stylist::deleteAll();
+        }
+
+        function test_save()
          {
+            //Arrange
+            $name = "Sara";
+            $test_Stylist = new Stylist($name);
+            $test_Stylist->save();
 
+            //Act
+            $result = Stylist::getAll();
+
+            //Assert
+            $this->assertEquals($test_Stylist, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = "Sara";
+            $name2 = "Brian";
+            $test_Stylist = new Stylist($name);
+            $test_Stylist->save();
+            $test_Stylist2 = new Stylist($name2);
+            $test_Stylist2->save();
+
+            //Act
+            $result = Stylist::getAll();
+
+            //Assert
+            $this->assertEquals([$test_Stylist, $test_Stylist2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $type = "Sara";
+            $type2 = "Brian";
+            $test_Stylist = new Stylist($name);
+            $test_Stylist->save();
+            $test_Stylist2 = new Stylist($name2);
+            $test_Stylist2->save();
+
+            //Act
+            Stylist::deleteAll();
+            $result = Stylist::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
          }
-
-
      }
-
  ?>
