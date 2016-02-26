b373623ff6d7fc44a3a8e75a9e69526e0f42cfc8
Blake Scott <Blakerscott@gmail.com>
2016-02-26 10:44:49 -0800

    add client test file

---
+++ tests/ClientTest.php
@@ -0,0 +1,16 @@
++<?php
+ +
+ +    require_once "src/Restaurant.php";
+ +
+ +    class RestaurantTest extends PHPUnit_Framework_TestCase
+ +    {
+ +
+ +        function test_method_breakitdown()
+ +        {
+ +
+ +        }
+ +
+ +
+ +    }
+ +
+ +?>
